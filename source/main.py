2ea9b31a05c596c18714f2887011938c93fe5d6b
Cominam <54367564+lastime1650@users.noreply.github.com>
2025-05-14 10:40:28 +0900

    Update main.py

--- source/main.py
+++ source/main.py
@@ -8,7 +8,7 @@ import struct
 import binascii
 
 
-def handle_packet(pkt):
+def handle_packet(pkt):# 핸들러
     print(pkt)
     
     pkt.get_payload()
@@ -17,9 +17,9 @@ def handle_packet(pkt):
     
     # TCP 체크
     IP_packet = IP(Original_Packet_Binary)
-    if IP_packet.haslayer("IP"):
+    if IP_packet.haslayer("IP"): # IP인가? 
         
-        TCP_packet = IP_packet["TCP"]
+        TCP_packet = IP_packet["TCP"] # TCP 추출 -> 이 경우, if문으로 적절하게 확인하는 것을 추천합니다.
         
         if ( str(IP_packet["IP"].dst) == "192.168.220.1" ):
             # DNAT
@@ -27,7 +27,7 @@ def handle_packet(pkt):
             # Ex -> In
             print( "DNAT ..\n " + IP_packet.dst ) # 변경 전
         
-            IP_packet["IP"].dst = "10.0.3.10" # 하드코딩,,,, 
+            IP_packet["IP"].dst = "10.0.3.10" # 하드코딩,,,,  ( 10.0.3.10 은 현재 포트포워딩된 내부 호스트 IP입니다. )
             print( IP_packet.dst ) # 변경 후
 
         elif ( str(IP_packet["IP"].src) == "10.0.3.10" ):
@@ -37,7 +37,7 @@ def handle_packet(pkt):
             # Internal ==> External
             print( "SNAT .. \n" + IP_packet.src ) # Before
 
-            IP_packet["IP"].src = "192.168.220.1"
+            IP_packet["IP"].src = "192.168.220.1" # ( 192.168.220.1 은 외부 통신 네트워크 인터페이스에 할당된 IP입니다. )
 
             print( IP_packet.src ) # After 
         
@@ -45,26 +45,24 @@ def handle_packet(pkt):
 
 
         # 패킷 바이너리가 달라졌기에,, 체크섬 무결성 값은 깨짐
-        # 삭제!
+        # 삭제하고, bytes()시 체크섬을 다시 구함
 
-        del IP_packet["IP"].chksum
+        del IP_packet["IP"].chksum 
         del TCP_packet.chksum
         
-        pkt.set_mark(1234) # 마크 적용 -> iptables - POSTROUTING에서 이를 확인할 것
-        
-        pkt.set_payload( bytes(IP_packet) ) # 실제 패킷까지 적용 하기
+        pkt.set_payload( bytes(IP_packet) ) # 바이너리 변환(체크섬  다시구함 ) set_payload로 변경사항 적용
         
         
         
-    pkt.accept()
+    pkt.accept() # 다시 iptables로 패킷 복귀
     return 
 
-QUEUE_NUM = 0
+QUEUE_NUM = 0 # NFQUEUE --queue-num 0 일 때
 
 nfqueue = NetfilterQueue()
 nfqueue.bind(
-    QUEUE_NUM,
-    handle_packet
+    QUEUE_NUM, # NFQUEUE 큐 넘버
+    handle_packet # 핸들러 함수
 )
 nfqueue.run()
 
